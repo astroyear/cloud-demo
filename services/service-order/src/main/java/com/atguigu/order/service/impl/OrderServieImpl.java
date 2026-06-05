5cb386c8384a1ab750d1c8da0973b986ca8fb636
year <164157127@qq.com>
2026-06-05 20:29:47 +0800

    springcloudAlibaba中的Nacos组件，用于注册中心。这里是注册中心的服务注册与服务发现的原理代码

---
+++ services/service-order/src/main/java/com/atguigu/order/service/impl/OrderServieImpl.java
@@ -0,0 +1,71 @@
+package com.atguigu.order.service.impl;
+
+import com.atguigu.order.bean.Order;
+import com.atguigu.order.service.OrderService;
+import com.atguigu.product.bean.Product;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
+@Slf4j
+@Service
+public class OrderServieImpl implements OrderService {
+
+    @Autowired
+    DiscoveryClient discoveryClient;
+    @Autowired
+    RestTemplate restTemplate;
+    @Autowired
+    LoadBalancerClient loadBalancerClient;
+
+    @Override
+    public Order createOrder(Long productId, Long userId) {
+        Order order = new Order();
+        order.setId(1L);
+        order.setUserId(userId);
+        order.setNickName("zhangsan");
+        order.setAddress("home");
+
+        Product product = getProductFromRemoteWithLoaderBalancerAnnotation(productId);
+        order.setProductList(Arrays.asList(product));
+        order.setTotalAmount(product.getPrice().multiply(new BigDecimal(product.getNum())));
+        return order;
+    }
+
+    private Product getProductFromRemoteWithLoaderBalancerAnnotation(Long productId){
+//        ServiceInstance choose = loadBalancerClient.choose("service-product");
+//        String url = "http://" + choose.getHost()+":" + choose.getPort() + "/product/"+productId;
+//        log.info("远程请求路径：{}"+url);
+
+        String url = "http://service-product/product/" + productId;
+        Product product = restTemplate.getForObject(url, Product.class);
+        return product;
+    }
+
+    private Product getProductFromRemoteWithLoaderBalancer(Long productId){
+        ServiceInstance choose = loadBalancerClient.choose("service-product");
+        String url = "http://" + choose.getHost()+":" + choose.getPort() + "/product/"+productId;
+        log.info("远程请求路径：{}"+url);
+        Product product = restTemplate.getForObject(url, Product.class);
+        return product;
+    }
+
+    private Product getProductFromRemote(Long productId){
+        List<ServiceInstance> instances = discoveryClient.getInstances("service-product");
+        ServiceInstance instance = instances.get(0);
+        String url = "http://" + instance.getHost()+":" + instance.getPort() + "/product/"+productId;
+        log.info("远程请求路径：{}"+url);
+        Product product = restTemplate.getForObject(url, Product.class);
+        return product;
+    }
+
+
+}
